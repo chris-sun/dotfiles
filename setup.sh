21b426eccf70d40021bba64747bd18cef629f5c6
Chris Sun <christophersun@hotmail.com>
2013-06-07 16:31:09 -0700

    improving the setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # After downloading the dotfiles directory
 # run this script (setup.sh)
 # to create symlinks to some files in this directory
-cd ..
-cp dotfiles ~
+DOTFILES_DIR=`pwd`
+ln -s ${DOTFILES} ~/dotfiles
 
 cd ~
 ln -s ~/dotfiles/bash/bash_profile .bash_profile
