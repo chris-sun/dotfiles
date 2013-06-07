b25560422ddfbd04f6ec88fd1a06e6dae58bd4bd
Chris Sun <christophersun@hotmail.com>
2013-06-07 16:38:49 -0700

    more changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # run this script (setup.sh)
 # to create symlinks to some files in this directory
 DOTFILES_DIR=`pwd`
-ln -s ${DOTFILES} ~/dotfiles
 
 cd ~
+ln -s ${DOTFILES_DIR} dotfiles
 ln -s ~/dotfiles/bash/bash_profile .bash_profile
 ln -s ~/dotfiles/vim/vimrc .vimrc
